39d494feae7a0dc1b7cee0e122bb30d376c4c5a2
Jasmine Chen <jasmine1226@gmail.com>
2019-08-10 23:49:49 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,6 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    show = self.find_by("rating = ?", self.highest_rating)
+    show = self.find_by(rating: self.highest_rating)
   end
 end
